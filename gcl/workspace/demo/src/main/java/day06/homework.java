78abd4e506498032974555abe803a9f50e4b1fa3
gclsgithub <36355840+gclsgithub@users.noreply.github.com>
2023-03-13 21:16:07 +0900

    update

--- gcl/workspace/demo/src/main/java/day06/homework.java
+++ gcl/workspace/demo/src/main/java/day06/homework.java
@@ -20,22 +20,43 @@ public class homework {
         strArr[6] = "ggggg";
 
         int x = 1;
-        for (String str :strArr){
+        for (String str : strArr) {
             System.out.println("第" + x + "项的值是:" + str);
             x++;
         }
 
-        String[] strArr1 = new String[]{strArr[2] + strArr[5],strArr[2] + strArr[6],strArr[2] + strArr[7]};
+        String[] strArr1 = new String[]{strArr[2] + strArr[5], strArr[2] + strArr[6], strArr[2] + strArr[7]};
 
-        for (int i =0 ;i<strArr1.length;i++){
-            System.out.println("第" + (i+1) + "项的值是:" + strArr1[i]);
+        for (int i = 0; i < strArr1.length; i++) {
+            System.out.println("第" + (i + 1) + "项的值是:" + strArr1[i]);
         }
 
         int j = 1;
-        for (String str :strArr1){
+        for (String str : strArr1) {
             System.out.println("第" + j + "项的长度是:" + str.length());
             j++;
         }
 
+        //作业5：
+        //题目：判断一个整数是几位数，并按照逆序输出。
+        //考点：流程控制、运算符、数组
+        //难度：中
+        System.out.println("作业5");
+        int number = 1234;
+        String numberStr = String.valueOf(number);
+        if (number >9 && number < 100){
+            System.out.println("十位数");
+        } else if (number >0 && number < 10){
+            System.out.println("个位数");
+        } else if (number >=100 && number < 1000){
+            System.out.println("百位数");
+        } else {
+            System.out.println("其他位数");
+        }
+
+        //switch
+        for (int i = numberStr.length() - 1; i >= 0; i--) {
+            System.out.print(numberStr.charAt(i));
+        }
     }
 }
