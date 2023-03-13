f0b3ff5ea8bde03a1fecaad2ced351d6f8c26d9f
koukou0709 <127408401+koukou0709@users.noreply.github.com>
2023-03-14 06:07:58 +0800

    0313作業上傳

--- pcl-study-member/koukou/untitled/src/main/java/org/demo0313/homework0313.java
+++ pcl-study-member/koukou/untitled/src/main/java/org/demo0313/homework0313.java
@@ -7,34 +7,124 @@ public class homework0313 {
 //    题目：有一对兔子，从出生后第3个月起每个月都生一对兔子，小兔子长到第三个月后每个月又生一对兔子。假如兔子都不死，要求输出一年内兔子的数量是多少。
 //    考点：流程控制
 //    难度：中
-        int a = 2;
-        int b = 2;
-//        for (int b = 3 ; b <= 12 ; b++){
-//            for (int a = 2 ; a > 2 ; a=+2)
-        if (b<=12){
-            int c = a + 2;
-            int d = b + 1;
-            System.out.println("兔子=");
+    System.out.println("-----作業一-----");
+        int a = 0;
+        System.out.print("1月2隻,");
+        for (int b = 2 ; b <= 12 ; b++) {
+            if (b <= 3){
+                a = a + 2;
+            } else {
+                a = a + (b - 3) * 2;
+            }
+            System.out.print(b + "月" + a + "隻,");
         }
+
 //    作业2：
 //    题目：判断10-105之间有多少个素数，并输出所有素数。【素数又称为质数，定义为在大于1的自然数中，除了1和它本身以外不再有其他因数的数】
 //    考点：流程控制、运算符
 //    难度：中
+    System.out.println("");
+    System.out.println("-----作業二-----");
+    System.out.print("質數=");
+        for (int c = 10 ; c <= 105 ; c++) {
+            if ( c%2 > 0 ){
+                int d = c;
+                if ( d%3 > 0){
+                    int e = d;
+                    if ( e%5 > 0){
+                        int f = e;
+                        if (f%7 > 0){
+                            int g = f;
+                            System.out.print(g+",");
+                        }
+                    }
+                }
+            }
+        }
 //    作业3：
 //    题目：判断100到500之间，哪些数是水仙花数。【水仙花数是指一个 n 位正整数 ( n≥3 )，它的每个位上的数字的 n 次幂之和等于它本身。（例如：1^3 + 5^3+ 3^3 = 153）】
 //    考点：流程控制、运算符
 //    难度：中
+    System.out.println("");
+    System.out.println("-----作業三-----");
+    System.out.print("水仙花數=");
+            for (int h = 0 ; h <= 9 ; h++){
+                for (int i = 0 ; i <= 9 ; i++){
+                    for (int j = 0 ; j <= 9 ; j++){
+                        if ( 500 >= h*h*h + i*i*i + j*j*j & h*h*h + i*i*i + j*j*j >=100){
+                            int k = h*h*h + i*i*i + j*j*j;
+                            if ( k == h*100 + i*10 +j){
+                                int l = k;
+                            System.out.print(l + ",");
+                            }
+                        }
+                    }
+                }
+            }
 //    作业4：
 //    题目：求1-100之间，有哪些数是完数。【完全数（Perfect number），又称完美数或完备数，是一些特殊的自然数。它所有的真因子（即除了自身以外的约数）的和（即因子函数），恰好等于它本身。例如：6=1+2+3】
 //    考点：流程控制、运算符
 //    难度：中
+    System.out.println("");
+    System.out.println("-----作業四-----");
+    System.out.print("完數=");
+    //Math.pow(10, 2);10的2次方 stuvwxyz Math.pow(2, s) * (Math.pow(2, t) - 1);
+        for (int s = 1 ; s <= 10 ; s++){
+            int[] t = {2,3,5,7,11};
+            for (int u:t){
+                double v = Math.pow(2, s) * (Math.pow(2, u) - 1);
+                if (100 >= v & v >= 1){
+                    double w = v;
+                    double y = 0;
+                    for (double x = 1 ; y <= 100 ; x++){
+                        y = y + x;
+                        if ( y == w){
+                            double z = y;
+                            System.out.print(z + ",");
+                        }
+                    }
+                }
+            }
+        }
+
 //    作业5：
 //    题目：判断一个整数是几位数，并按照逆序输出。
 //    考点：流程控制、运算符、数组
 //    难度：中
+    System.out.println("");
+    System.out.println("-----作業五-----");
+        int p = 123456;
+        String q = String.valueOf(p);
+        System.out.println(q.length()+"位數");
+//        int[] i1 = new int[i02.length()];
+//        String[] str2 = {str1};
+        for (int r = q.length()-1; r >= 0 ; r--){
+            System.out.print(q.charAt(r));
+        }
+//        降序??
+//        Arrays.sort(a,Collections.reverseOrder());
+//        int[] intArr = {1,3,5,7,9};
+//        for (int i = 0; i < intArr.length; i++){
+//            System.out.println(intArr[i]);
+//        }
 //    作业6：
 //    题目：输出2000年到3000年中的闰年。
 //    考点：流程控制、运算符
 //    难度：中
+    System.out.println("");
+    System.out.println("-----作業六-----");
+    System.out.println("閏年：");
+        for (int m = 2000 ; m <= 3000 ; m++){
+            if ( m % 4 == 0 ){
+                int n = m;
+                if ( n % 100 > 0){
+                    int o = n;
+                    System.out.println(o);
+                } else if( n % 400 == 0){
+                    int o = n;
+                    System.out.println(o);
+                }
+            }
+        }
     }
 }
