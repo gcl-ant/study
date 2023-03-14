a5d140376fd8d9f3ee4f09ed3e0b18bad5cd4e06
255023zjy <z18240153856@gmail.com>
2023-03-14 23:05:32 +0900

    20230314作业day06

---
+++ pcl-study-member/zhangjiayi/homework/homework/src/main/java/day07/homework03.java
@@ -0,0 +1,12 @@
+package day07;
+
+public class homework03 {
+    public static void main(String[] args) {
+        for(int i6=2000;i6<=3000;i6++) {
+            if(i6%4==0 ) {
+                System.out.println(+i6+"是闰年");
+            }
+        }
+
+    }
+}
