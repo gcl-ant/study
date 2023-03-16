daf432fdb5da475fc2c847a983a954d42f692b3b
koukou0709 <127408401+koukou0709@users.noreply.github.com>
2023-03-16 17:38:49 +0800

    作業修改

---
+++ pcl-study-member/koukou/untitled/src/main/java/Demo03/Demo0314/Text031402.java
@@ -0,0 +1,8 @@
+package Demo03.Demo0314;
+
+public class Text031402 extends Text031401 {
+    public Text031402(){
+        super.age = 20;
+    }
+
+}
---
+++ pcl-study-member/koukou/untitled/src/main/java/Demo03/Demo0314/Text031403.java
@@ -0,0 +1,8 @@
+package Demo03.Demo0314;
+
+public class Text031403 {
+    public static void main(String[] args) {
+        Text031402 a = new Text031402();
+//        System.out.println(text031401.age);
+    }
+}
