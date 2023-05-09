4bbe70e95e39c9ae56af2d6e130602e7f6a2e9a5
gclsgithub <36355840+gclsgithub@users.noreply.github.com>
2023-05-09 21:59:00 +0900

    SQL format

--- gcl/workspace/onlineshop/onlineshop/src/main/java/com/pcl/onlineshop/dao/OlUserMapper.java
+++ gcl/workspace/onlineshop/onlineshop/src/main/java/com/pcl/onlineshop/dao/OlUserMapper.java
@@ -9,7 +9,16 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OlUserMapper {
 
-    @Select("SELECT id FROM user WHERE mail = #{mail} AND password = #{password} AND is_delete != 1")
+    @Select(" SELECT " +
+            "       id " +
+            " FROM " +
+            "       user " +
+            " WHERE " +
+            "     mail = #{mail} " +
+            " AND " +
+            "     password = #{password} " +
+            " AND " +
+            "     is_delete != 1")
     Integer queryMailAndPassword(@Param("mail") String mail, @Param("password") String password);
 
     @Select("SELECT * FROM user where id = #{id}")
