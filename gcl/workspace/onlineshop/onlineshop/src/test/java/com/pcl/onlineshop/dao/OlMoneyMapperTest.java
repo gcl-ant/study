deda92f2b33605c2f2471e2432dfd8c4af1c8acc
gclsgithub <36355840+gclsgithub@users.noreply.github.com>
2023-05-15 21:32:30 +0900

    HYC 入出金履歴

---
+++ gcl/workspace/onlineshop/onlineshop/src/test/java/com/pcl/onlineshop/dao/OlMoneyMapperTest.java
@@ -0,0 +1,32 @@
+package com.pcl.onlineshop.dao;
+
+import com.pcl.onlineshop.dto.entity.MoneyEntity;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class OlMoneyMapperTest {
+
+    @Autowired
+    private OlMoneyMapper olMoneyMapper;
+
+    @Test
+    void selectOlMoneyMapperTime001(){
+        List<MoneyEntity> test = olMoneyMapper.selectOpeMoneyHistory(20230001);
+        assert test.size() == 2;
+    }
+
+    @Test
+    void selectOlMoneyMapperTime002(){
+        List<MoneyEntity> test = olMoneyMapper.selectOpeMoneyHistory(20230000);
+        assert test.size() == 0;
+    }
+}
