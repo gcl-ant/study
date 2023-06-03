0f061d1ba54a0e843037ceff5ee58e096bce733f
gcl-ant <124421363+gcl-ant@users.noreply.github.com>
2023-06-03 10:31:26 +0900

    Update OnlineshopApplicationTests.java

--- gcl/workspace/onlineshop/onlineshop/src/test/java/com/pcl/onlineshop/OnlineshopApplicationTests.java
+++ gcl/workspace/onlineshop/onlineshop/src/test/java/com/pcl/onlineshop/OnlineshopApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OnlineshopApplicationTests {
+class OnlineshopApplicationTests {//kkk
 
     @Test
     void contextLoads() {
